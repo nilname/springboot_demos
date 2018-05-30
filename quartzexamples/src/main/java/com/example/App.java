e2a96850442585724456887ee25b7a5c7eb77c18
nilname <980548079@qq.com>
2018-05-30 11:18:34 +0800

    update

---
+++ quartzexamples/src/main/java/com/example/App.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@EnableScheduling
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
